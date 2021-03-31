d26536b0f670774cc697dffa1912fa4e9b018917
pocketknife <reynaldlawrence@outlook.ph>
2021-03-31 18:13:47 +0800

    initial commit

---
+++ src/Components/ImageSection.js
@@ -0,0 +1,43 @@
+import React from "react";
+import about from "../img/fn_mandalorian.PNG";
+
+function ImageSection() {
+  return (
+    <div className="ImageSection">
+      <div className="img">
+        <img src={about} alt="" />
+      </div>
+      <div className="about-info">
+        <h4>
+          I am<span> Lorem Ipsum</span>
+        </h4>
+        <p className="about-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          necessitatibus perferendis numquam dolorem cumque facilis. Desir per
+          po la ta doso vi slone ve?
+        </p>
+        <div className="about-details">
+          <div className="left-section">
+            <p>Full Name</p>
+            <p>Age</p>
+            <p>Nationality</p>
+            <p>Languages</p>
+            <p>Address</p>
+            <p>Countries</p>
+          </div>
+          <div className="right-section">
+            <p>: Lorem Ipsum</p>
+            <p>: 37</p>
+            <p>: Filipino</p>
+            <p>: Filipino, Japanese, English</p>
+            <p>: 2 Beightbridge, BN7 GB9, United kingdom</p>
+            <p>: France, Spain, Germany</p>
+          </div>
+        </div>
+        <button className="btn">Download Cv</button>
+      </div>
+    </div>
+  );
+}
+
+export default ImageSection;
